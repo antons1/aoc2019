0b6b453d0811642565fb224d5e6a29e7d24649b9
Håkon Antonsen <hakon.antonsen@telia.no>
2019-12-04 12:32:21 +0100

    Day 3 part 2

--- day3/solution.js
+++ day3/solution.js
@@ -2,7 +2,7 @@ const input = require('./testinput.json');
 const fs = require('fs');
 
 const realInput = fs.readFileSync('./input.txt', { encoding: 'utf8' });
-//input.push({ input: realInput, result: "x" })
+input.push({ input: realInput, result: "x" })
 
 function translateInputString(inputString) {
     return inputString.split("\n").map((line) =>
@@ -12,63 +12,52 @@ function translateInputString(inputString) {
         }))
 }
 
-function getCoordsAndCurrentPosition(initX, initY, {direction, length}) {
-    switch(direction) {
-        case "U": return { from: initY, to: initY + length, y: initY + length, x: initX}
-        case "D": return { from: initY - length, to: initY, y: initY - length, x: initX}
-        case "R": return { from: initX, to: initX + length, y: initY, x: initX + length}
-        case "L": return { from: initX - length, to: initX, y: initY, x: initX - length}
-    }
-}
-
-function getXCoords(from, to, y) {
-    const coords = [];
-    for(let i = from; i <= to; i++) coords.push(`${i},${y}`);
-    return coords;
-}
-
-function getYCoords(from, to, x) {
-    const coords = [];
-    for(let i = from; i <= to; i++) coords.push(`${x},${i}`);
-    return coords;
-}
-
 function getAllCoords(instructionLine) {
-    let curX = 0;
-    let curY = 0;
+    let x = 0;
+    let y = 0;
 
     return instructionLine.map((instruction) => {
-        const {from, to, y, x} = getCoordsAndCurrentPosition(curX, curY, instruction);
-        curX = x;
-        curY = y;
-        switch(instruction.direction) {
-            case "U": case "D": return getYCoords(from, to, x);
-            case "R": case "L": return getXCoords(from, to, y);
+        const coords = [];
+        for(let i = 1; i <= instruction.length; i++) {
+            switch(instruction.direction) {
+                case "U": coords.push(`${x},${++y}`); break;
+                case "D": coords.push(`${x},${--y}`); break;
+                case "R": coords.push(`${++x},${y}`); break;
+                case "L": coords.push(`${--x},${y}`); break;
+            }
+            //console.log(`Direction ${instruction.direction}, length ${instruction.length} => ${x},${y}`)
         }
+        return coords;
     }).reduce((acc, curr) => acc.concat(curr));
 }
 
 function getLowestCollision(collisions) {
-    let manhattans = collisions.map((coll) => {
-        const match = coll.match(/(-?\d*),(-?\d*)/);
+    let manhattans = collisions.map(({coord, length}) => {
+        const match = coord.match(/(-?\d*),(-?\d*)/);
         const [ _, x, y ] = match;
         return Math.abs(parseInt(x)) + Math.abs(parseInt(y));
-    }).sort((a, b) => parseInt(a) > parseInt(b));
+    }).sort((a, b) => a-b);
     return { manhattans, lowest: manhattans[0] }
 }
 
-function doInstructionSet({input, result}) {
+function doInstructionSet({input, result, length}) {
     const parsedInput = translateInputString(input);
     const coords = getAllCoords(parsedInput[0]);
     const coords2 = getAllCoords(parsedInput[1]);
-    coords.shift();
-    coords2.shift();
-    const collisions = coords.filter((coord) => -1 !== coords2.indexOf(coord));//getCollisions(parsedInput[1], coords);
+    const collisions = [];
+    coords.forEach((coord, index) => {
+        const collision = coords2.indexOf(coord);
+        if(collision !== -1) {
+            collisions.push({ coord, length: index + collision + 2});
+        }
+    })
     const {manhattans, lowest} = getLowestCollision(collisions);
+    const {coords: shortestDistanceCoords, length: shortestDistance } = collisions.sort((a, b) => a.length - b.length)[0];
     //console.log(coords);
-    console.log(collisions);
-    console.log(manhattans);
-    console.log(`${result} ? => ${lowest}`);
+    //console.log(coords2)
+    console.log(collisions.sort((a, b) => a.length - b.length));
+    //console.log(manhattans);
+    console.log(`${result} ? => ${lowest} && ${length} ? => ${shortestDistance}`);
 }
 
 input.forEach(doInstructionSet);
